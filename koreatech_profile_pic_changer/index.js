08590d60e864722e9a326fe00bd684563ca4882e
Mozin <temiumaru@naver.com>
2025-04-14 20:18:10 +0900

    한기대 프사 변경

---
+++ koreatech_profile_pic_changer/index.js
@@ -0,0 +1,6 @@
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a,i)=>{
+    if (a.alt=="사람이름 사진"||a.alt=="사람이름")
+        a.src='https://github.com/TemiTem2/pictures/blob/main/1235.png?raw=true'
+
+})
